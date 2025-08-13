aec94093f3f561ae561241186631ef0b7979faaa
Rizal <84571944+theonlyrizal@users.noreply.github.com>
2025-08-13 23:18:06 +0600

    complete problem 1

--- problem1.js
+++ problem1.js
@@ -1,3 +1,7 @@
 function totalFine(fare) {
-  // You have to write your code here
+  if (typeof fare !== 'number' || fare <= 0) {
+    return 'Invalid';
+  } else {
+    return fare + fare * 0.2 + 30;
+  }
 }
