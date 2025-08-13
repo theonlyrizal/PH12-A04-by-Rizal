5eb0ac60c0498eebfe03c7632cc6d8e6401e6a71
Rizal <84571944+theonlyrizal@users.noreply.github.com>
2025-08-14 00:25:02 +0600

    complete problem 4

--- problem4.js
+++ problem4.js
@@ -1,3 +1,21 @@
 function isSame(arr1, arr2) {
-  // You have to write your code here
+  if (Array.isArray(arr1) === false || Array.isArray(arr2) === false) {
+    return 'Invalid';
+  } else {
+    let flag = true;
+    if (arr1.length !== arr2.length) {
+      flag = false;
+      return flag;
+    } else {
+      for (let index = 0; index < arr1.length; index++) {
+        if (arr1[index] === arr2[index]) {
+          continue;
+        } else {
+          flag = false;
+          break;
+        }
+      }
+      return flag;
+    }
+  }
 }
