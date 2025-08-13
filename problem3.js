603a19668a04c26c630c4efbd8e85db75bffadb9
Rizal <84571944+theonlyrizal@users.noreply.github.com>
2025-08-13 23:52:58 +0600

    complete problem 3

--- problem3.js
+++ problem3.js
@@ -1,3 +1,19 @@
 function bestTeam(player1, player2) {
-  // You have to write your code here
+  if (typeof player1 !== 'object' || typeof player2 !== 'object') {
+    return 'Invalid';
+  } else {
+    if (
+      player1['foul'] + player1['cardY'] + player1['cardR'] >
+      player2['foul'] + player2['cardY'] + player2['cardR']
+    ) {
+      return player2['name'];
+    } else if (
+      player1['foul'] + player1['cardY'] + player1['cardR'] <
+      player2['foul'] + player2['cardY'] + player2['cardR']
+    ) {
+      return player1['name'];
+    } else {
+      return 'Tie';
+    }
+  }
 }
