ce54c2169f3f138d6751d902d8c6a403322de550
Rizal <84571944+theonlyrizal@users.noreply.github.com>
2025-08-13 23:36:25 +0600

    complete problem 2

--- problem2.js
+++ problem2.js
@@ -1,3 +1,7 @@
 function onlyCharacter(str) {
-  // You have to write your code here
+  if (typeof str !== 'string') {
+    return 'Invalid';
+  } else {
+    return str.split(' ').join('').toUpperCase();
+  }
 }
