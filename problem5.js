3e8abb8f0c8c048a38523b4b02814b842cc8d25d
Rizal <84571944+theonlyrizal@users.noreply.github.com>
2025-08-14 03:09:04 +0600

    complete problem 5

--- problem5.js
+++ problem5.js
@@ -1,3 +1,22 @@
 function resultReport(marks) {
-  // You have to write your code here
+  if (Array.isArray(marks) !== true) {
+    return 'Invalid';
+  } else {
+    let object = { finalScore: 0, pass: 0, fail: 0 };
+    if (marks.length === 0) {
+      return object;
+    }
+    for (let index = 0; index < marks.length; index++) {
+      object['finalScore'] += marks[index];
+      if (marks[index] < 40) {
+        object['fail']++;
+      } else {
+        object['pass']++;
+      }
+    }
+    object['finalScore'] = Math.round(object['finalScore'] / marks.length);
+    return object;
+  }
 }
+
+console.log(resultReport([-546]));
